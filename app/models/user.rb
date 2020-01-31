09df70c1340f49b919ecac7b6825d9c156182384
olav35 <fosseolav@gmail.com>
2020-01-31 12:47:39 +0100

    add post

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
+  has_many :post
   validates :name, presence: true, uniqueness: true, length: { maximum: 50 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_31_102452) do
+ActiveRecord::Schema.define(version: 2020_01_31_113843) do
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "user_id"
+    t.text "link"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.text "name"
