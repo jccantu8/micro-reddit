5dbc56d1bfad6eaa4d81458ca3c05509ab909ed5
olav35 <fosseolav@gmail.com>
2020-01-31 12:54:57 +0100

    add post validations

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: 'fossegrim')
+    @post = @user.post.new(link: 'http://fossegr.im')
+  end
+
+  test 'should be valid' do
+    assert @post.valid?
+  end
+
+  test 'link should be present' do
+    @post.link = ' '
+    assert_not @post.valid?
+  end
 end
